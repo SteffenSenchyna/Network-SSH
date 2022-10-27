6efea3ed3aaa50656c8c59b0b9f0b372601b65b6
SteffenSenchyna <steffen@cash2crypto.ca>
2022-10-27 11:10:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
 import os
+import json
 from datetime import time
 from random import randint
-
+import pathlib
 from PyQt5.QtCore import Qt, QEventLoop, QTimer, QThread, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QMessageBox, QVBoxLayout, \
     QTabWidget, QInputDialog, QLabel, QLineEdit, QStatusBar, QProgressBar
@@ -24,6 +25,7 @@ class Window(QWidget):
         # Create a top-level layout
         layout = QVBoxLayout()
         self.setLayout(layout)
+        self.directoryCheck()
         # Show loaded files
         self.conflabel = QLabel(self)
         self.conflabel.setText("Command File: %s" % self.filepath_cmd)
@@ -41,12 +43,21 @@ class Window(QWidget):
         layout.addWidget(tabs)
         layout.addStretch()
 
+    def directoryCheck(self):
+        """Checks to see if there is proper file structure and creates directories if needed"""
+        self.directory = str(pathlib.Path(__file__).parent.resolve())+"\IPs"
+        try:
+            os.makedirs(self.directory)
+        except FileExistsError:
+            # directory already exists
+            pass
+
+
     def networkTabUI(self):
         """Network Configuration page UI."""
         NetConfTab = QWidget()
         layout = QVBoxLayout()
         """Buttons for the conf menu"""
-
         # setting menu to the button
         b1 = QPushButton("Load a command file")
         b1.clicked.connect(self.on_click_get_file_path_cmd)
@@ -66,24 +77,31 @@ class Window(QWidget):
         """Network Back Up page UI."""
         NetMonTab = QWidget()
         layout = QVBoxLayout()
-        b1 = QPushButton('Load a IP file')
-        b1.clicked.connect(self.on_click_get_file_path_ip)
-        layout.addWidget(b1)
-        b2 = QPushButton("Deploy")
-        b2.clicked.connect(self.takeinputs2)
-        layout.addWidget(b2)
+        bScan = QPushButton('Scan Network')
+        bScan.clicked.connect(self.on_click_get_file_path_ip)
+        bLoad = QPushButton('Load a IP file')
+        bLoad.clicked.connect(self.on_click_get_file_path_ip)
+        bDeploy = QPushButton("Backup")
+        bDeploy.clicked.connect(self.saveConfig)
+        bErase = QPushButton("Erase")
+        bDeploy.clicked.connect(self.eraseConfig)
+        layout.addWidget(bScan)
+        layout.addWidget(bLoad)
+        layout.addWidget(bDeploy)
+        layout.addWidget(bErase)
         layout.addWidget(self.IPlabel2)
         NetMonTab.setLayout(layout)
         return NetMonTab
 
     def on_click_get_file_path_ip(self):
-        path = QFileDialog.getOpenFileName(self, 'Open a file', 'IPFiles/',
+        path = QFileDialog.getOpenFileName(self, 'Open a file', 'IPs/',
                                            'All Files (*.*)')
         if path != ('', ''):
-            self.filepath_ip = os.path.basename(path[0])
-            self.IPlabel.setText("IP File: %s" % self.filepath_ip)
+            self.filepath_ip_label = os.path.basename(path[0])
+            self.filepath_ip = path[0]
+            self.IPlabel.setText("IP File: %s" % self.filepath_ip_label)
             self.IPlabel.setAlignment(Qt.AlignBottom)
-            self.IPlabel2.setText("IP File: %s" % self.filepath_ip)
+            self.IPlabel2.setText("IP File: %s" % self.filepath_ip_label)
             self.IPlabel2.setAlignment(Qt.AlignBottom)
 
 
@@ -116,7 +134,7 @@ class Window(QWidget):
             'username': '%s' % self.username,
             'password': '%s' % self.password
         }
-        ipfile = open('IPFiles/%s' % self.filepath_ip)
+        ipfile = open(self.directory + '\IPs\%s' % self.filepath_ip)
         for line in ipfile:
             device['host'] = line.strip("\n")
             print("Connecting to ", line)
@@ -135,14 +153,15 @@ class Window(QWidget):
                 print("Disconnecting")
                 net_connect.disconnect()
         print('Commands have been deployed')
-
-    def takeinputs2(self):
+    #Function for saving configs
+    def saveConfig(self):
+        #Error checking if there is no IP file loaded
         if self.filepath_ip == 'None':
             dlg = QMessageBox(self)
             dlg.setWindowTitle("Error")
             dlg.setText("Please load a IP file")
             button = dlg.exec()
-
+        #If no error the promts user for login credentials for network devices
         else:
             self.username, done1 = QInputDialog.getText(
                 self, 'Input Dialog', 'Enter your username:')
@@ -155,16 +174,58 @@ class Window(QWidget):
             'username': '%s' % self.username,
             'password': '%s' % self.password
         }
-        ipfile = open('IPFiles/%s' % self.filepath_ip)
-        for line in ipfile:
-            device['host'] = line.strip("\n")
-            print("Connecting to ", line)
+        #Loading IP file JSON and converting to python dictionary 
+        ipfile = open(self.filepath_ip)
+        ipJSON = json.load(ipfile)
+        #Iterating over the dictionary and grabbing the IPS and then deploying the show run command
+        #to the networking devices these outputs are saved to .txt files
+        for i in ipJSON:
+            device['host'] = ipJSON[i].strip("\n")
+            print("Connecting to ", ipJSON[i])
             net_connect = ConnectHandler(**device)
             loop = QEventLoop()
             QTimer.singleShot(2000, loop.quit)
             outp = net_connect.send_command('show run')
-            print(line.strip("\n"), " configuration has been saved")
-            with open('Configs/' + line.strip("\n") + '.txt', 'w') as f:
+            print(ipJSON[i].strip("\n"), " configuration has been saved")
+            with open('Configs/' + ipJSON[i].strip("\n") + '.txt', 'w') as f:
+                f.write(outp)
+            print("Disconnecting")
+            net_connect.disconnect()
+        print('Devices have been backed up')
+    #Function for erasing configs 
+    def eraseConfig(self):
+        #Error checking if there is no IP file loaded
+        if self.filepath_ip == 'None':
+            dlg = QMessageBox(self)
+            dlg.setWindowTitle("Error")
+            dlg.setText("Please load a IP file")
+            button = dlg.exec()
+        #If no error the promts user for login credentials for network devices
+        else:
+            self.username, done1 = QInputDialog.getText(
+                self, 'Input Dialog', 'Enter your username:')
+
+            self.password, done2 = QInputDialog.getText(
+                self, 'Input Dialog', 'Enter your password:', QLineEdit.Password)
+        device = {
+            'device_type': 'cisco_ios',
+            'host': 'ip',
+            'username': '%s' % self.username,
+            'password': '%s' % self.password
+        }
+        #Loading IP file JSON and converting to python dictionary 
+        ipfile = open(self.filepath_ip)
+        ipJSON = json.load(ipfile)
+        #Iterating over the dictionary and grabbing the IPS and then deploying the write erase command
+        for i in ipJSON:
+            device['host'] = ipJSON[i].strip("\n")
+            print("Connecting to ", ipJSON[i])
+            net_connect = ConnectHandler(**device)
+            loop = QEventLoop()
+            QTimer.singleShot(2000, loop.quit)
+            outp = net_connect.send_command('show run')
+            print(ipJSON[i].strip("\n"), " configuration has been saved")
+            with open('Configs/' + ipJSON[i].strip("\n") + '.txt', 'w') as f:
                 f.write(outp)
             print("Disconnecting")
             net_connect.disconnect()
